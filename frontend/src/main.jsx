7f62740b031cd04a9640d0dec3370172fba5aa83
Kaan Özkan <168916101+ozzaii@users.noreply.github.com>
2025-02-19 00:06:26 +0300

    Use HashRouter for GitHub Pages compatibility

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react'
+import { HashRouter as Router } from 'react-router-dom'
 import App from './App.jsx'
+import theme from './theme'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <Router>
+        <App />
+      </Router>
+    </ChakraProvider>
+  </React.StrictMode>,
 )
